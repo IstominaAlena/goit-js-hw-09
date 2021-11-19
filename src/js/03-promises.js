180017e73ec69ec0a72ef8f1f5a37b1fa39219dc
Istomina Alena <mynikisalena@gmail.com>
2021-11-19 12:13:30 +0200

    Все сделано

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,42 +1,30 @@
 import throttle from 'lodash.throttle';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const refs = {
-  form: document.querySelector('.form'),
-  delayInput: document.querySelector('input[name="delay"]'),
-  stepInput: document.querySelector('input[name="step"]'),
-  amountInput: document.querySelector('input[name="amount"]'),
-  button: document.querySelector('button[type="submit"]'),
-};
+const form = document.querySelector('.form');
 
-refs.form.addEventListener('input', throttle(localStorageFn, 500));
-
-const STORAGE_KEY = 'promise-key';
-const dataObj = {};
-
-function localStorageFn() {
-  dataObj[refs.delayInput.name] = refs.delayInput.value;
-  dataObj[refs.stepInput.name] = refs.stepInput.value;
-  dataObj[refs.amountInput.name] = refs.amountInput.value;
-
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(dataObj));
-}
-
-const savedData = localStorage.getItem(STORAGE_KEY);
-const parsedData = JSON.parse(savedData);
-
-refs.form.addEventListener('submit', onFormSubmit);
+form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  refs.button.disabled = true;
-  const { delay, step, amount } = parsedData;
-  let delayValue = Number(delay);
-  const stepValue = Number(step);
-  const amountValue = Number(amount);
+
+  const formElements = new FormData(evt.currentTarget);
+  const dataObj = {};
+
+  formElements.forEach((value, name) => (dataObj[name] = value));
+
+  let delayValue = Number(dataObj.delay);
+  const stepValue = Number(dataObj.step);
+  const amountValue = Number(dataObj.amount);
 
   for (let i = 1; i <= amountValue; i += 1) {
-    createPromise(i, delayValue);
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        onSuccessFn({ position, delay });
+      })
+      .catch(({ position, delay }) => {
+        onErrorFn({ position, delay });
+      });
     delayValue += stepValue;
   }
 }
@@ -47,26 +35,18 @@ function createPromise(position, delay) {
 
     setTimeout(() => {
       if (shouldResolve) {
-        resolve(onSuccessFn(position, delay));
+        resolve({ position, delay });
       } else {
-        reject(onErrorFn(position, delay));
+        reject({ position, delay });
       }
     }, delay);
   });
 }
 
-function onSuccessFn(position, delay) {
+function onSuccessFn({ position, delay }) {
   Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
 }
 
-function onErrorFn(position, delay) {
+function onErrorFn({ position, delay }) {
   Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
 }
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
